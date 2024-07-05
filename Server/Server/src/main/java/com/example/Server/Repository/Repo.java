33b6ffb0e635a0fa66c3033b90607e83c7f07c89
Austin Teffo <82168002+AustinTeffo@users.noreply.github.com>
2024-07-05 23:03:42 +0200

    Delete Server directory

--- Server/Server/src/main/java/com/example/Server/Repository/Repo.java
+++
@@ -1,7 +0,0 @@
-package com.example.Server.Repository;
-
-import com.example.Server.Models.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface Repo extends CrudRepository<User,Integer> {
-}
